9c03c2c86d316d0fa4f46f30637d47091217087d
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2022-08-24 01:12:26 -0300

    Add MustVerifyEmail e add middleware verified in routes

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('/task', App\Http\Controllers\TaskController::class)->middleware('auth');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+->name('home')->middleware('verified');
+Route::resource('/task', App\Http\Controllers\TaskController::class)->middleware('auth', 'verified');
 
 Route::get('/email-message', function() {
     return new MessageTestMail();
